e7a28096e57fb4a984e12a68dadc7e6cd04e6b56
abjose <ajose@caltech.edu>
2014-12-26 16:35:35 -0800

    add libs

--- tests/base/TransformTests.ts
+++ tests/base/TransformTests.ts
@@ -1,7 +1,13 @@
 
 module Tests {
-  export class CreationTests extends tsUnit.TestClass {
 
+  function pointsNearlyEqual(p1: Entities.Point, p2: Entities.Point) {
+    // test passed points are approximately equal
+    return p1.distance(p2) < 0.001;
+  }
+  
+  export class CreationTests extends tsUnit.TestClass {
+    
     verifyOriginalPointsArentModifiedDuringTransform() {
       var t = new Base.Transform();
       t.translate(2, 2);
@@ -13,8 +19,12 @@ module Tests {
       t.transform(p1);
       t.transform(p2);
 
-      this.areIdentical([p1.x, p1.y], [0, 1]); // better way?
-      this.areIdentical([p2.x, p2.y], [-1, 1]);
+      // expected points
+      var ex_p1 = new Entities.Point(0, 1);
+      var ex_p2 = new Entities.Point(-1, 1);
+
+      this.isTrue(pointsNearlyEqual(ex_p1, p1));
+      this.isTrue(pointsNearlyEqual(ex_p2, p2));
     }
     
     transformPoint() {
@@ -25,9 +35,15 @@ module Tests {
 
       var p1 = t.transform(new Entities.Point(0, 1));
       var p2 = t.transform(new Entities.Point(-1, 1));
+      console.log(p1, p2);
+      
+      // expected points
+      var ex_p1 = new Entities.Point(-3, -4.5);
+      var ex_p2 = new Entities.Point(-1.5, -4.5);
+      // things seem to apply backwards...
 
-      this.areIdentical([p1.x, p1.y], []);
-      this.areIdentical([p2.x, p2.y], []);
+      this.isTrue(pointsNearlyEqual(ex_p1, p1));
+      this.isTrue(pointsNearlyEqual(ex_p2, p2));
     }
 
     testInversionUndoesTransform() {
@@ -39,9 +55,14 @@ module Tests {
 
       var p1 = ti.transform(t.transform(new Entities.Point(0, 1)));
       var p2 = ti.transform(t.transform(new Entities.Point(-1, 1)));
+      
+      // expected points
+      var ex_p1 = new Entities.Point(0, 1);
+      var ex_p2 = new Entities.Point(-1, 1);
 
-      this.areIdentical([p1.x, p1.y], [0, 1]);
-      this.areIdentical([p2.x, p2.y], [-1, 1]);
+      // switch args, not that it matters
+      this.isTrue(pointsNearlyEqual(ex_p1, p1));
+      this.isTrue(pointsNearlyEqual(ex_p2, p2));
     }
   }
 }
