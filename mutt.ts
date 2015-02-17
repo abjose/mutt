ccebc91ab3c962a753e39fe4797f858546fbfe81
Alex Jose <ajose@caltech.edu>
2015-02-16 23:12:19 -0500

    move TransformHierarchy to own file

--- mutt.ts
+++ mutt.ts
@@ -7,6 +7,7 @@
 /// <reference path="base/Style.ts" />
 /// <reference path="base/StyleManager.ts" />
 /// <reference path="base/Transform.ts" />
+/// <reference path="base/TransformHierarchy.ts" />
 /// <reference path="base/TransformStack.ts" />
 /// <reference path="entities/Line.ts" />
 /// <reference path="entities/Point.ts" />
@@ -17,8 +18,10 @@
 
 /*
 TODO:
-- CHANGE MUTT to use scene graph rather than expecting entities to have
-  their own transforms defined.
+- make manipulating transform hierarchy less annoying
+- reeeeeally need to improve canvas performance
+  could group on render order, or even group everything that doesn't
+  intersect another entity (if that's pretty fast)
 
 Minimal steps to prototype bootstrapping interface-creator:
 - create woof so very easy to introspect/modify event settings
@@ -47,138 +50,19 @@ number of interpretations of 'children'.
 
 mutt 2du:
 - add spatial relation stuff to Rect and Line?
+  and now can use scene graph! so... spatial relation stuff will take
+  ...two entities in scene graph? or assume to be on one entity?
+  kinda like having spatial relation stuff defined externally, fits with 
+  theme...even if it's a theme you don't like that much...
 - add simple spatial query to MultiIndex
 - add clipping
 - make view
 - think about messenger stuff
 */
 
-interface EntityToNodeMap { [entity_id: string]: TransformNode; }
-class TransformHierarchy {
-  // TODO: decide if can have multiple copies of a single entity in hierarchy
-  // TODO: decide if tree or DAG or whatever, add checks for that.
-  root: TransformNode;
-  entity_to_node: EntityToNodeMap;
-  
-  constructor() {
-    this.root = new TransformNode();
-    this.entity_to_node = {};
-  }
-
-  add_entity(entity: Base.Entity) {
-    this.entity_to_node[entity.id] = new TransformNode();
-    this.entity_to_node[entity.id].parent = this.root;
-  }
-  
-  remove_entity(entity: Base.Entity) {
-    var node = this.get_node(entity);
-    node.parent.remove_child(node);
-    delete this.entity_to_node[entity.id];
-    this.entity_to_node[entity.id] = undefined;  // necessary?
-  }
-  
-  get_node(entity: Base.Entity): TransformNode {
-    return this.entity_to_node[entity.id];
-  }
-
-  set_relation(parent: Base.Entity, child: Base.Entity) {
-    // TODO: check to make sure adding relation won't ruin graph/tree
-    var parent_node = this.get_node(parent);
-    var child_node  = this.get_node(child);
-    // remove old relation
-    child_node.parent.remove_child(child_node);
-    // create new relation
-    parent_node.add_child(child_node);
-    child_node.set_parent(parent_node);
-  }
-  
-  get_transform(entity: Base.Entity) {
-    return this.get_node(entity).transform;
-  }
-
-  get_common_ancestor(a: TransformNode, b: TransformNode): TransformNode {
-    var seen = {};  // seen node ids
-    while (true) {
-      if (a.id in seen) return a;
-      seen[a.id] = true;
-      if (b.id in seen) return b;
-      seen[b.id] = true;
-      if (a.id != this.root.id) a = a.parent;
-      if (b.id != this.root.id) b = b.parent;
-    }
-  }
-
-  get_path(a: TransformNode, b: TransformNode): TransformNode[] {
-    // Build paths to common ancestor.
-    var ca = this.get_common_ancestor(a, b);
-    var a_path = [], b_path = [];
-    while (a.id != ca.id) {
-      a_path.push(a);
-      a = a.parent;
-    }
-    while (b.id != ca.id) {
-      b_path.push(b);
-      b = b.parent;
-    }
-
-    // Append paths and return.
-    return a_path.concat(ca, b_path.reverse());
-  }
-    
-  transform_between(a: Base.Entity, b: Base.Entity): Base.Transform {
-    var path = this.get_path(this.get_node(a), this.get_node(b));
-    return this.transform_from_path(path);
-  }
-
-  transform_from_root(entity: Base.Entity) {
-    var path = this.get_path(this.root, this.get_node(entity));
-    return this.transform_from_path(path); 
-  }
-  
-  transform_from_path(path: TransformNode[]) {
-    return path.reduce(function(transform, node) {
-      return transform.multiply(node.transform);
-    }, new Base.Transform());
-  }
-}
-
-interface ChildrenMap { [node_id: string]: TransformNode }
-class TransformNode {
-  // TODO: do some kind of cacheing
-  transform: Base.Transform;
-  parent: TransformNode;
-  children: ChildrenMap;
-  id: string;
-  
-  constructor() {
-    this.transform = new Base.Transform();
-    this.parent = undefined;
-    this.children = {};
-    this.id = Utility.UUID();
-  }
-
-  set_parent(parent: TransformNode) {
-    this.parent = parent;
-  }
-  
-  add_child(child: TransformNode) {
-    this.children[child.id] = child;
-  }
-  
-  remove_child(child: TransformNode) {
-    this.children[child.id] = undefined;
-  }
-  
-  get_children() {
-    return Object.keys(this.children).map(function (id) {
-      return this.children[id];
-    });
-  }
-}
-
 var mutt = {
   entities: new Base.MultiIndex(),
-  scene: new TransformHierarchy(),  // better name?
+  scene: new Base.TransformHierarchy(),  // better name?
 
   update: function() {
     var entities_to_draw = this.entities.query();
