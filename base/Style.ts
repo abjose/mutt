2e0a0769609aada253d32bcb20b6be52c20beacc
Alex Jose <ajose@caltech.edu>
2015-02-25 00:00:05 -0500

    consolidate style code

--- base/Style.ts
+++ base/Style.ts
@@ -2,8 +2,36 @@
 module Base {
   export interface Style<EntityType> {
     style: string;
-    entity: string; // change these names...
+    type: string; // change these names...
     draw(entity: EntityType, transform: Base.Transform);
     // consider adding relations and stuff
   }
+
+   /*
+    Hmm, isn't this pretty specific to Paw? Things probably shouldn't be
+    so mixed together...
+   */
+  
+  interface StyleToDrawer { [style_type: string]: Base.Style<any>; }
+  interface StyleCollection { [entity_type: string]: StyleToDrawer; }
+  export class StyleManager {
+    styles: StyleCollection; // [entity_type][style_type] -> Style
+
+    constructor() {
+      this.styles = {};
+    }
+    
+    draw(entity, transform: Base.Transform) {
+      this.get_style(entity).draw(entity, transform);
+    }
+
+    get_style(entity) {
+      return this.styles[entity.type][entity.style];
+    }
+
+    add_style(style: Base.Style<any>) {
+      if (this.styles[style.type] == undefined) this.styles[style.type] = {};
+      this.styles[style.type][style.style] = style;
+    }
+  } 
 }
