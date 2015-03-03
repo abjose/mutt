7117ee32cc8caba0ec86904ee22f4af739a73b43
Alex Jose <ajose@caltech.edu>
2015-03-02 21:35:23 -0500

    small include changes

--- paw.ts
+++ paw.ts
@@ -1,9 +1,8 @@
 /// <reference path="libs/gl-matrix.d.ts" />
+/// <reference path="utility/uuid.ts" />
 /// <reference path="base/Entity.ts" />
 /// <reference path="base/Style.ts" />
-/// <reference path="base/StyleManager.ts" />
 /// <reference path="base/Transform.ts" />
-/// <reference path="base/TransformStack.ts" />
 /// <reference path="entities/Line.ts" />
 /// <reference path="entities/Point.ts" />
 /// <reference path="entities/Rectangle.ts" />
@@ -15,8 +14,7 @@
 var paw = {
   transform: new Base.TransformStack(),
   styles: new Base.StyleManager(),
-  //draw(entity: Entity) { // change entity to being type any?
-  draw(entity) {
+  draw(entity: Base.Entity) {
     this.styles.draw(entity, this.transform.get());
   },
 };
